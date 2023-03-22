410c439832e019643cec0f43d75cb8685d8425f9
fahad khan <fahadkhan09922@gmail.com>
2023-03-23 04:29:38 +0500

    update

--- subject.js
+++ subject.js
@@ -3,63 +3,63 @@ let subjects = [{
         lesson: "English Language",
         location: 'Burj Khalifa',
         price: "500",
-        spaces: 6,
+        spaces: 5,
         image: "img/englishlanguage.jpg"
     },
     {
         lesson: "Swimming",
         location: 'Burj Al Arab',
         price: "3000",
-        spaces: 9,
+        spaces: 5,
         image: "img/swimming.jpg"
     },
     {
         lesson: "Sports activity",
         location: 'Global Village',
         price: "4000",
-        spaces: 8,
+        spaces: 5,
         image: "img/sportactivity.png"
     },
     {
         lesson: "Ice skating",
         location: 'Dubai Mall',
         price: "1500",
-        spaces: 0,
+        spaces: 5,
         image: "img/icekating.png"
     },
     {
         lesson: "Futsal",
         location: 'Ski Dubai',
         price: "2000",
-        spaces: 19,
+        spaces: 5,
         image: "img/futsall.jpg"
     },
     {
         lesson: "Arabic",
         location: 'Dubai Garden',
         price: "3000",
-        spaces: 6,
+        spaces: 5,
         image: "img/arabic.jpeg"
     },
     {
         lesson: "Indoor Games",
         location: 'Down Town',
         price: "1000",
-        spaces: 3,
+        spaces: 5,
         image: "img/indoorgames.jpg"
     },
     {
         lesson: "Outdoor Games",
         location: 'Desert Safari',
         price: "800",
-        spaces: 14,
+        spaces: 5,
         image: "img/ouutdoorgames.png"
     },
     {
         lesson: "Movies",
         location: 'Dubai Mall',
         price: "500",
-        spaces: 1,
+        spaces: 5,
         image: "img/movies.jpg"
 
     },
@@ -67,7 +67,7 @@ let subjects = [{
         lesson: "Practice a Skill",
         location: 'Home town',
         price: "200",
-        spaces: 0,
+        spaces: 5,
         image: "img/practiceskils.png"
 
     },
